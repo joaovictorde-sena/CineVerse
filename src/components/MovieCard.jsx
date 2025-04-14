181bf90c8d3f749b4f4e2251909594cf4f86adac
João Victor <joaovictordesena2020@gmail.com>
2025-04-13 23:11:51 -0300

    update project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,8 @@ import {Link} from "react-router-dom"
 
 import {FaStar} from 'react-icons/fa'
 
+import "./MovieCard.css"
+
 const imageUrl = import.meta.env.VITE_IMG
 
 const MovieCard = ({movie, showLink = true}) => {
